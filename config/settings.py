975d31338dc9a4763cb325600235bf4d161826f1
정인우 <crossway7777@naver.com>
2022-11-20 11:47:25 +0900

    date modify

--- config/settings.py
+++ config/settings.py
@@ -101,13 +101,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'Asia/seoul'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
